9779deb90d15d0802b8bede52bdf19de88ac13d3
wahha2003 <104408266+wahha2003@users.noreply.github.com>
2024-04-12 10:44:47 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,30 +1,22 @@
 import React from "react";
-import { GithubOne, Home, Mail } from "@icon-park/react";
+import { Home, Mail } from "@icon-park/react";
 import CustomLink from "@/components/customLink";
 import Package from "../../package.json";
 
 const Footer = () => {
   // 加载配置
-  const githubName = import.meta.env.VITE_GITHUB_NAME;
   const homeUrl = import.meta.env.VITE_HOME_URL;
-  const emailUrl = import.meta.env.VITE_EMAIL_URL;
   const siteIcp = import.meta.env.VITE_SITE_ICP;
 
   return (
     <footer id="footer">
       <div className="social">
-        <CustomLink
-          iconDom={<GithubOne />}
-          to={`https://github.com/${githubName}/`}
-        />
         <CustomLink iconDom={<Home />} to={homeUrl} />
-        <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
       </div>
       <div className="text">
         <p>
           <CustomLink
             text={Package.alia}
-            to="https://github.com/imsyy/site-status"
           />
           &nbsp;Version&nbsp;{Package.version}
         </p>
